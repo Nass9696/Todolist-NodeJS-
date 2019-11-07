20545920b61fa3171b7161c8b4c456e36b37b17c
Nassim Gacem <dev.nassim.pro@gmail.com>
2019-11-07 12:15:05 +0100

    Create query POST, PUT, GET, DELETE + Test on POSTMAN : Successfully

--- app.js
+++ app.js
@@ -3,4 +3,20 @@ const express = require("express");
 const app = express();
 const port = 4000;
 
+app.get("/", (req, res) => {
+  res.send("Query GET Sucess");
+});
+
+app.post("/", (req, res) => {
+  res.send("Query POST Sucess");
+});
+
+app.put("/", (req, res) => {
+  res.send("Query PUT Sucess");
+});
+
+app.delete("/", (req, res) => {
+  res.send("Query DELETE Sucess");
+});
+
 app.listen(port);
