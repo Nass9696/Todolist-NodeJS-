b39e265e0b6867942e2bb2f79c81e9a1e01d8a7b
Nassim Gacem <dev.nassim.pro@gmail.com>
2019-11-11 20:27:05 +0100

    Add item in DB with post request

--- app.js
+++ app.js
@@ -70,10 +70,15 @@ app.get("/", (req, res) => {
 
 //Send form
 app.post("/", (req, res) => {
-  Item.findAll().then(items => {
-    console.log("All items :", JSON.stringify(items, null, 4));
-  });
-  res.send("Query POST Sucess");
+  Item.create({
+    item: req.body.item
+  })
+    .then(() => {
+      res.redirect("/");
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
 
 app.put("/", (req, res) => {
