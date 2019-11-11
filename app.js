a685e37e44a4ac377b11332f30aa7cae5f305fb8
Nassim Gacem <dev.nassim.pro@gmail.com>
2019-11-11 22:06:11 +0100

    Add eventlistener prompt + Link CDN axios

--- app.js
+++ app.js
@@ -57,6 +57,7 @@ app.get("/", (req, res) => {
         
       </div>
       <script src="edit.js"></script>
+      <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
     </body>
     </html>`);
   });
